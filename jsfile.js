ae00295688c84972c120e991b6f20b4b49f9e3bd
Vladhex12 <infohlfdz@gmail.com>
2022-07-28 20:04:48 +0100

    Final Thesis for home page completed

--- jsfile.js
+++ jsfile.js
@@ -1,3 +1,16 @@
-document.querySelector(".navmenu ul li").addEventListener("click", function(){
-    this.classList.toggle("active")
-});
+function release() {
+    document.getElementById("panel").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.navbtn')) {
+      var dropdowns = document.getElementsByClassName("navmenu");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
